189b97d61af7721b3908153fd9d2dd58ccc42461
Corwin <corwinvandalen@gmail.com>
2021-04-20 15:23:09 +0200

    Initial transfer page for erc20

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, HashRouter as Router, Switch } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Mintnonfungible } from "./pages/Mintnonfungible";
+import { Transferfungible } from "./pages/Transferfungible";
 
 
 export const App = () => {
@@ -9,6 +10,7 @@ export const App = () => {
       <Switch>
         <Route path="/" exact component={Home}></Route>
         <Route path="/mintnonfungible" exact component={Mintnonfungible}></Route>
+        <Route path="/transferfungible" exact component={Transferfungible}></Route>
       </Switch>
     </Router>
   )
--- src/pages/Transferfungible.tsx
+++ src/pages/Transferfungible.tsx
@@ -1,3 +1,7 @@
+import React from "react";
+
 export const Transferfungible = () => {
-    
+  return(
+    <p>Placeholder</p>
+  );
 }
